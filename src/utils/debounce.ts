b797bf9b1a7f15d428c377232187d79a6f5d9099
Hemendra <hemendraroy04@gmail.com>
2026-09-10 12:53:23 +0530

    Added debounce and throttle

---
+++ src/utils/debounce.ts
@@ -0,0 +1,11 @@
+export function debounce<T extends (...args: any[]) => void>(callback: T, delay: number) {
+  let timeoutId: ReturnType<typeof setTimeout>;
+
+  return (...args: Parameters<T>) => {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+}
